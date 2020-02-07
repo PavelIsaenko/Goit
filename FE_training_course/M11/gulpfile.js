ebd16c94e162a9994e73c16f20bb558c05b7df62
PavelIsaenko <pavelisaenko30@gmail.com>
2020-02-07 19:31:24 +0300

    transfer

---
+++ FE_training_course/M11/gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+
+function styles(cb) {
+   gulp.src('./scss/**/*.scss')
+       .pipe(sass({
+          outputStyle: 'compressed'
+       }))
+       .pipe(autoprefixer({
+            cascade: false
+        }))
+       .pipe(rename({
+          suffix: '.min'
+       }))
+       .pipe(gulp.dest('css/'))
+   cb();
+}
+
+gulp.task('default', styles);
